c28a892bdf96c9719a367108fb2e18278cadffca
suryadwprmn <suryadwipramana18@gmail.com>
2024-08-08 21:28:33 +0700

    perubahan pada port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
